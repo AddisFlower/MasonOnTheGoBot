2f4f86d3bbdfe0cc7937798d7277349a35f5278d
ntessera <ntessera@gmu.edu>
2021-06-23 21:36:05 -0400

    Added simple help command that tells the user about all the possible commands of this bot.

--- bot.py
+++ bot.py
@@ -11,4 +11,18 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(message):
+    if message.author.bot or message.author == client.user:
+        return
+    if message.content.startswith('!help'):
+        response = '**This bot has multiple commands**\n' \
+                   '**Chose from this list of commands:**\n' \
+                   '`!currentTemperature` - returns the current temperature in Fairfax\n' \
+                   '`!covidImmunizationRates` - returns the current covid vaccination rates in a specified state ' \
+                   '(Virginia is the default option) \n' \
+                   '`!currentEvents` - returns the current events happening in GMU\n' \
+                   '`!currentTempAtLocation` returns the temperature at the specified location\n'
+
+        await message.channel.send(response)
 client.run(TOKEN)
