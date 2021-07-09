4c1de1cf6f2f8b8f63a63301743b79ca09fe8d51
ntessera <ntessera@gmu.edu>
2021-07-09 16:50:40 -0400

    Updated the help command to include the newly added commands that are in the CustomEvents.py class.

--- help.py
+++ help.py
@@ -12,7 +12,21 @@ class MasonHelpCommand(commands.HelpCommand):
                    'If a zip-code is not specified, returns the temperature in Fairfax by default\n' \
                    '`!covidImmunizationRates` - returns the current covid vaccination rates in a specified state. ' \
                    'If a 2-letter state code is not specified, returns the rates in Virginia by default.\n' \
-                   '`!currentEvents` - returns the current events happening in GMU\n'
+                   '`!currentGMUEvents` - returns the current events happening in GMU\n\n' \
+                   '**The commands below pertain toward the custom event functionality of this bot**\n' \
+                   '`!createEvent` - creates a custom event with the given arguments that are separated by |.\n' \
+                   'Use the command like this: `!createEvent name | start date | end date | start time | end time | location | description`\n' \
+                   '`!displayEvents` - returns all the created events in the discord server. \n' \
+                   '`!setEventName` - sets the name of an event in the event list with the given id.\n ' \
+                   '`!setEventStartDate` - sets the start date of an event in the event list with the given id.\n' \
+                   '`!setEventEndDate` - sets the end date of an event in the event list with the given id.\n' \
+                   '`!setEventStartTime` - sets the start time of an event in the event list with the given id.\n' \
+                   '`!setEventEndTime` - sets the end time of an event in the event list with the given id.\n' \
+                   '`!setEventLocation` - sets the location of an event in the event list with the given id.\n' \
+                   '`!setEventDescription` - sets the description of an event in the event list with the given id.\n' \
+                   '`!setEventName` - sets the name of an event in the event list with the given id.\n' \
+                   '`!clearEvents` - clears all the events in the current event list.\n' \
+                   '`!removeEvent` - removes the event with the given id from the event list.'
         await self.get_destination().send(response)
 
     async def send_command_help(self, command):
