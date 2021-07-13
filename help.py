e8f5cb3f1cf3796eb105b2c6d0239c7b031eb566
nritter <nritter@gmu.edu>
2021-07-12 22:56:56 -0400

    Separated string into two messages to deal with discord message length limits

--- help.py
+++ help.py
@@ -14,24 +14,26 @@ class MasonHelpCommand(commands.HelpCommand):
                    'If a zip-code is not specified, returns the temperature in Fairfax by default\n' \
                    '`!covidImmunizationRates` - returns the current covid vaccination rates in a specified state. ' \
                    'If a 2-letter state code is not specified, returns the rates in Virginia by default.\n' \
-                   '`!currentGMUEvents` - returns the current events happening in GMU\n\n' \
-                   '**The commands below pertain toward the custom event functionality of this bot**\n' \
-                   '`!createEvent` - creates a custom event with the given arguments that are separated by |.\n' \
-                   'Use the command like this: `!createEvent name | start date | end date | start time | end time | location | description`\n' \
-                   '`!displayEvents` - returns all the created events in the discord server. \n' \
-                   '`!setEventName` - sets the name of an event in the event list with the given id.\n ' \
-                   '`!setEventStartDate` - sets the start date of an event in the event list with the given id.\n' \
-                   '`!setEventEndDate` - sets the end date of an event in the event list with the given id.\n' \
-                   '`!setEventStartTime` - sets the start time of an event in the event list with the given id.\n' \
-                   '`!setEventEndTime` - sets the end time of an event in the event list with the given id.\n' \
-                   '`!setEventLocation` - sets the location of an event in the event list with the given id.\n' \
-                   '`!setEventDescription` - sets the description of an event in the event list with the given id.\n' \
-                   '`!setEventName` - sets the name of an event in the event list with the given id.\n' \
-                   '`!clearEvents` - clears all the events in the current event list.\n' \
-                   '`!removeEvent` - removes the event with the given id from the event list.\n' \
+                   '`!currentGMUEvents` - returns the current events happening in GMU\n' \
                    '`!traffic` - begins sending live notifications for traffic events in Northern Virginia according to 511northernVA.\n' \
-                   '`!stopTraffic` - ends the live notification announcements for the traffic command so no new events are sent.'
+                   '`!stopTraffic` - ends the live notification announcements for the traffic command so no new events are sent.\n\n' \
+                   '**The commands below pertain toward the custom event functionality of this bot**'
+        custom = '`!createEvent` - creates a custom event with the given arguments that are separated by |.\n' \
+                 'Use the command like this: `!createEvent name | start date | end date | start time | end time | location | description`\n' \
+                 '`!displayEvents` - returns all the created events in the discord server. \n' \
+                 '`!setEventName` - sets the name of an event in the event list with the given id.\n ' \
+                 '`!setEventStartDate` - sets the start date of an event in the event list with the given id.\n' \
+                 '`!setEventEndDate` - sets the end date of an event in the event list with the given id.\n' \
+                 '`!setEventStartTime` - sets the start time of an event in the event list with the given id.\n' \
+                 '`!setEventEndTime` - sets the end time of an event in the event list with the given id.\n' \
+                 '`!setEventLocation` - sets the location of an event in the event list with the given id.\n' \
+                 '`!setEventDescription` - sets the description of an event in the event list with the given id.\n' \
+                 '`!setEventName` - sets the name of an event in the event list with the given id.\n' \
+                 '`!clearEvents` - clears all the events in the current event list.\n' \
+                 '`!removeEvent` - removes the event with the given id from the event list.'
+
         await self.get_destination().send(response)
+        await self.get_destination().send(custom)
 
     async def send_command_help(self, command):
         return self.get_destination().send(command.description)
