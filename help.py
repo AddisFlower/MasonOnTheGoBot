2abac0f313e2136214add102c604ba33199f56d2
ntessera <ntessera@gmu.edu>
2021-07-13 14:18:56 -0400

    Modified help command to be more informative

--- help.py
+++ help.py
@@ -8,11 +8,12 @@ class MasonHelpCommand(commands.HelpCommand):
     async def send_bot_help(self, mapping):
         response = '**Welcome to the Mason On The Go Bot!**\n' \
                    '**Your one stop shop for weather, events, traffic, and more!**\n' \
+                   '**This bot has daily notifications for the weather forecast in Fairfax and covid immunization rates in Virginia**\n' \
                    '**This bot has multiple commands**\n' \
                    '**Chose from this list of commands:**\n' \
-                   '`!currentTemp` - returns the current temperature in the specified zip-code. ' \
+                   '`!currentTemp [zip-code]` - returns the current temperature in the specified zip-code. ' \
                    'If a zip-code is not specified, returns the temperature in Fairfax by default\n' \
-                   '`!covidImmunizationRates` - returns the current covid vaccination rates in a specified state. ' \
+                   '`!covidImmunizationRates [state code]` - returns the current covid vaccination rates in a specified state. ' \
                    'If a 2-letter state code is not specified, returns the rates in Virginia by default.\n' \
                    '`!currentGMUEvents` - returns the current events happening in GMU\n' \
                    '`!traffic` - begins sending live notifications for traffic events in Northern Virginia according to 511northernVA.\n' \
@@ -21,16 +22,16 @@ class MasonHelpCommand(commands.HelpCommand):
         custom = '`!createEvent` - creates a custom event with the given arguments that are separated by |.\n' \
                  'Use the command like this: `!createEvent name | start date | end date | start time | end time | location | description`\n' \
                  '`!displayEvents` - returns all the created events in the discord server. \n' \
-                 '`!setEventName` - sets the name of an event in the event list with the given id.\n ' \
-                 '`!setEventStartDate` - sets the start date of an event in the event list with the given id.\n' \
-                 '`!setEventEndDate` - sets the end date of an event in the event list with the given id.\n' \
-                 '`!setEventStartTime` - sets the start time of an event in the event list with the given id.\n' \
-                 '`!setEventEndTime` - sets the end time of an event in the event list with the given id.\n' \
-                 '`!setEventLocation` - sets the location of an event in the event list with the given id.\n' \
-                 '`!setEventDescription` - sets the description of an event in the event list with the given id.\n' \
-                 '`!setEventName` - sets the name of an event in the event list with the given id.\n' \
-                 '`!clearEvents` - clears all the events in the current event list.\n' \
-                 '`!removeEvent` - removes the event with the given id from the event list.'
+                 '`!setEventName [eventID]` - sets the name of an event in the event list with the given id.\n ' \
+                 '`!setEventStartDate [eventID]` - sets the start date of an event in the event list with the given id.\n' \
+                 '`!setEventEndDate [eventID]` - sets the end date of an event in the event list with the given id.\n' \
+                 '`!setEventStartTime [eventID]` - sets the start time of an event in the event list with the given id.\n' \
+                 '`!setEventEndTime [eventID]` - sets the end time of an event in the event list with the given id.\n' \
+                 '`!setEventLocation [eventID]` - sets the location of an event in the event list with the given id.\n' \
+                 '`!setEventDescription [eventID]` - sets the description of an event in the event list with the given id.\n' \
+                 '`!setEventName [eventID]` - sets the name of an event in the event list with the given id.\n' \
+                 '`!clearEvents ` - clears all the events in the current event list.\n' \
+                 '`!removeEvent [eventID]` - removes the event with the given id from the event list.'
 
         await self.get_destination().send(response)
         await self.get_destination().send(custom)
