a913d91d53930ae4f26c1f4a4b3d2744b56efc63
ntessera <ntessera@gmu.edu>
2021-07-18 13:16:24 -0400

    Added the command that lets a user specify which channel the notifications and traffic updates should be sent in. Also, added the command that lets the user chose at what time the notifications are sent.

--- cogs/ChannelSetup.py
+++ cogs/ChannelSetup.py
@@ -13,8 +13,23 @@ class ChannelSetup(commands.Cog):
         channel = discord.utils.get(ctx.guild.channels, name=channel_name)
         channel_id = channel.id
         self.bot.channel_id = channel_id
-        await self.bot.get_channel(channel_id).send("Notifications and traffic updates will be sent in this channel now!")
-        print(channel_id)
+        await self.reload_extensions()
+        response = "Notifications and traffic updates will be sent in the " + channel_name + " channel now!"
+        await ctx.send(response)
+
+    @commands.command(name='setNotificationTime', description='Sets the time the two notifications are sent (Time must be sent in military time).')
+    async def set_time(self, ctx, hour, minutes):
+        self.bot.WHEN = time(int(hour), int(minutes), 0)
+        await self.reload_extensions()
+        await ctx.send("Time has been successfully changed!")
+
+    async def reload_extensions(self):
+        self.bot.unload_extension(f'cogs.Traffic')
+        self.bot.unload_extension(f'cogs.Covid')
+        self.bot.unload_extension(f'cogs.Weather')
+        self.bot.load_extension(f'cogs.Traffic')
+        self.bot.load_extension(f'cogs.Covid')
+        self.bot.load_extension(f'cogs.Weather')
 
 
 def setup(bot):
