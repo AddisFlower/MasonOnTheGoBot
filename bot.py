cf8f1bbbe627d7f153ca36db2c670941efccc51c
ntessera <1654-ntessera@users.noreply.git.gmu.edu>
2021-07-18 05:05:59 +0000

    Revert "Modified the on_ready function so that the bot sends a message in the general..."
    This reverts commit 93ff6c5b7f5957cfa1a88bd9211f96136830fbe0

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 from help import MasonHelpCommand
 
 load_dotenv()
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-channel_id = 0
+TOKEN = os.getenv('TOKEN')
+
 bot = commands.Bot(command_prefix='!', help_command=MasonHelpCommand())
 
 if __name__ == '__main__':
@@ -22,12 +22,9 @@ if __name__ == '__main__':
 @bot.event
 async def on_ready():
     """Instructions for whenever the bot is running"""
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='your instructions :)'))
+    await bot.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.listening, name='your instructions :)'))
     print(f'{bot.user} has connected to Discord!')
-
-    # set the default notification channel as the general channel
-    channel = discord.utils.get(bot.get_all_channels(), name="general")
-    await bot.get_channel(channel.id).send("Type !help to learn about what I do!")
 
 
 @bot.event
@@ -38,13 +35,4 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("Please enter the required arguments when using this command.")
 
-
-@bot.command(name='setNotificationChannel', description="Sets the channel where the notifications and traffic updates are sent.")
-async def set_channel(ctx, *, channel_name):
-    global channel_id
-    channel = discord.utils.get(ctx.guild.channels, name=channel_name)
-    channel_id = channel.id
-    await bot.get_channel(channel_id).send("Notifications and traffic updates will be sent in this channel now!")
-    print(channel_id)
-
-bot.run(DISCORD_TOKEN)
+bot.run(TOKEN)
